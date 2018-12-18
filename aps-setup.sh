d0e5ef8030c7fc80c4c813646072e3beb2e7eaa0
Diogo Neto <36502099+Mentolz@users.noreply.github.com>
2018-12-18 09:30:52 +0000

    Update aps-setup.sh

--- aps-setup.sh
+++ aps-setup.sh
@@ -7,3 +7,8 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome*.deb
 sudo apt-get install spotify-client
+apt-get install zsh
+apt-get install git-core
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+sudo shutdown -r 0
